6b8fe9a7944be310712349d2b5ae148e8ffdf6ca
Abigail Tandiwe Simelane <tandisimelane24@gmail.com>
2026-08-20 00:00:16 +0300

    final version external-api

--- external_api.py
+++ external_api.py
@@ -6,7 +6,7 @@ HEADERS = {"User-Agent": "InventoryApp/1.0 (tandisimelane24@gmail.com)"}
 
 
 def get_product_by_barcode(barcode):
-    url = f"{BASE_URL}/api/v3.6/product/{barcode}.json"
+    url = f"{BASE_URL}/api/v2/product/{barcode}.json"
     response = requests.get(url, headers=HEADERS, timeout=5)
     response.raise_for_status()
     result = response.json()
